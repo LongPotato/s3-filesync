33fa3295be46c91152771dc9b958a0805f21d1c3
Miraj Patel <mspatel@cpp.edu>
2017-01-23 15:04:48 -0800

    refgdfsg

--- watch.js
+++ watch.js
@@ -1,10 +1,26 @@
-var fs = require('fs');
+var watchr = require('watchr');
 
-fs.watch('dropbox', function (event, filename) {
-    console.log('event is: ' + event);
-    if (filename) {
-        console.log('filename provided: ' + filename);
-    } else {
-        console.log('filename not provided');
+
+exports.watch = function() {
+  var path = process.cwd() + '/dropbox';
+  function listener (changeType, fullPath, currentStat, previousStat) {
+    switch (changeType) {
+      case 'update':
+        console.log('the file', fullPath, 'was updated', currentStat, previousStat)
+        break
+      case 'create':
+        console.log('the file', fullPath, 'was created', currentStat)
+        break
+      case 'delete':
+        console.log('the file', fullPath, 'was deleted', previousStat)
+        break
     }
-});
+  }
+
+  function next (err) {
+    if ( err )  return console.log('watch failed on', path, 'with error', err)
+    console.log('watch successful on', path)
+  }
+
+  var stalker = watchr.open(path, listener, next);
+}
