71eb590b0cd71c74536827c66062fbea69925e1d
LongPotato <bkh4nh@gmail.com>
2017-01-23 15:47:45 -0800

    changed watch test

--- watch.js
+++ watch.js
@@ -1,26 +1,25 @@
 var watchr = require('watchr');
 
+var path = process.cwd() + '/dropbox';
+function listener (changeType, fullPath, currentStat, previousStat) {
+  console.log(changeType);
+  switch (changeType) {
 
-exports.watch = function() {
-  var path = process.cwd() + '/dropbox';
-  function listener (changeType, fullPath, currentStat, previousStat) {
-    switch (changeType) {
-      case 'update':
-        console.log('the file', fullPath, 'was updated', currentStat, previousStat)
-        break
-      case 'create':
-        console.log('the file', fullPath, 'was created', currentStat)
-        break
-      case 'delete':
-        console.log('the file', fullPath, 'was deleted', previousStat)
-        break
-    }
+    case 'update':
+      console.log('the file', fullPath, 'was updated', currentStat, previousStat)
+      break
+    case 'create':
+      console.log('the file', fullPath, 'was created', currentStat)
+      break
+    case 'delete':
+      console.log('the file', fullPath, 'was deleted', previousStat)
+      break
   }
-
-  function next (err) {
-    if ( err )  return console.log('watch failed on', path, 'with error', err)
-    console.log('watch successful on', path)
-  }
-
-  var stalker = watchr.open(path, listener, next);
 }
+
+function next (err) {
+  if ( err )  return console.log('watch failed on', path, 'with error', err)
+  console.log('watch successful on', path)
+}
+
+var stalker = watchr.open(path, listener, next);
