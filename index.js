1638682b031f67fb36b4a2d1787951aed24e70a7
Miraj Patel <mspatel@cpp.edu>
2017-01-23 17:08:12 -0800

    changed port

--- index.js
+++ index.js
@@ -54,8 +54,8 @@ function deleteChanges(fileName){
 });
 }
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
 
 	var path = process.cwd() + '/dropbox';
 	function listener (changeType, fullPath, currentStat, previousStat) {
